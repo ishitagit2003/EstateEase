b95e4ab7dda4aa7ce84ae3d8638196aef69a0e99
ishitagit2003 <ishitagupta2003@gmail.com>
2024-05-14 16:35:00 +0530

    Navbar ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Navbar from "./components/navbar/Navbar"
+import "./layout.scss"
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="layout">
+      <Navbar/>
+    </div>
   )
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import "./navbar.scss";
+import React, { useState } from 'react';
+
+function Navbar(){
+  const [open,setOpen]= useState(false)
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>EstateEase</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+      <a href="/">Sign in</a>
+        <a href="/" className="register">Sign up</a>
+      </div>
+      <div className="menuIcon">
+        <img src="/menu.png" alt="" onClick={()=> setOpen((prev) =>!prev)}/>
+      </div>
+      <div className={open? "menu active" :"menu"}>
+      <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+        <a href="/">Sign in</a>
+        <a href="/">Sign up</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar

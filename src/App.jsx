85566ff3f32d6c607cafaa80f80207c3e9a4247a
ishitagit2003 <ishitagupta2003@gmail.com>
2024-05-15 15:05:21 +0530

    Home page ui changed ans search bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./components/navbar/Navbar"
+import HomePage from "./routes/homePage/homePage"
 import "./layout.scss"
+
 function App() {
   return (
     <div className="layout">
+      <div className="navbar">
       <Navbar/>
+      </div>
+      <div className="content">
+      <HomePage/>
+      </div>
     </div>
   )
 }

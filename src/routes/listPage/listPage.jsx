18387c303949c517cba2bb70a7e5b2cc680521e9
ishitagit2003 <ishitagupta2003@gmail.com>
2024-05-19 18:27:12 +0530

    list work

---
+++ src/routes/listPage/listPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./listPage.scss"
+
+function ListPage() {
+  return (
+    <div className='listPage'>
+      listPage
+    </div>
+  )
+}
+
+export default ListPage

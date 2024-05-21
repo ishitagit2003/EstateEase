cc402797f5536d7e5fbd0090aa8f141cfc70d64b
ishitagit2003 <ishitagupta2003@gmail.com>
2024-05-21 17:40:32 +0530

    layout and dummy data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import Navbar from "./components/navbar/Navbar"
 import HomePage from "./routes/homePage/homePage"
-import "./layout.scss"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+import Login from "./routes/login/login";
+
 
 function App() {
+  const router = createBrowserRouter([
+  {
+    path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+      ]
+  }
+  ]);
   return (
-    <div className="layout">
-      <div className="navbar">
-      <Navbar/>
-      </div>
-      <div className="content">
-      <HomePage/>
-      </div>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 

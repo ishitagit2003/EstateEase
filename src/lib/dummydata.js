7f1d720e59e5d082501a83d59da6b919a5b20429
ishitagit2003 <ishitagupta2003@gmail.com>
2024-05-23 20:27:13 +0530

    Datalist

--- src/lib/dummydata.js
+++ src/lib/dummydata.js
@@ -10,6 +10,7 @@ export const listData = [
       latitude: 51.5074,
       longitude: -0.1278,
     },
+    
     {
       id: 2,
       title: "An Awesome Apartment Near the Park! Almost too good to be true!",

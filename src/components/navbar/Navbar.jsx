cd48ad1d2079af77dd672a57ae8b146f7e5de8a3
ishitagit2003 <ishitagupta2003@gmail.com>
2024-06-07 03:25:27 +0530

    chatbox and post option added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import "./navbar.scss";
-import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 
-function Navbar(){
-  const [open,setOpen]= useState(false)
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const user = true;
   return (
     <nav>
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>EstateEase</span>
+          <span>LamaEstate</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
@@ -16,22 +19,44 @@ function Navbar(){
         <a href="/">Agents</a>
       </div>
       <div className="right">
-      <a href="/">Sign in</a>
-        <a href="/" className="register">Sign up</a>
-      </div>
-      <div className="menuIcon">
-        <img src="/menu.png" alt="" onClick={()=> setOpen((prev) =>!prev)}/>
-      </div>
-      <div className={open? "menu active" :"menu"}>
-      <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">Agents</a>
-        <a href="/">Sign in</a>
-        <a href="/">Sign up</a>
+        {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>John Doe</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/">Sign in</a>
+            <a href="/" className="register">
+              Sign up
+            </a>
+          </>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;

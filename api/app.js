1a41610c1083241e9e21e34814d2d3e1cf7cd922
ishitagit2003 <ishitagupta2003@gmail.com>
2024-06-17 23:20:12 +0530

    mongoose connection

--- api/app.js
+++ api/app.js
@@ -1,11 +1,23 @@
 import express from "express";
-import authRoute from "./routes/auth.route.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+// import authRoute from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
 
+console.log('Database URL:', process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => console.log("Mongodb connected!"))
+  .catch((error) => console.error('Error connecting to Mongodb:', error));
+
+
 app.use(express.json());
 
-app.use("/api/auth", authRoute);
+// app.use("/api/auth", authRoute);
 
 app.listen(8800, ()=>{
     console.log("Server is running");

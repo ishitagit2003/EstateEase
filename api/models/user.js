095fddc947a0da2453854f0ed556ddfac5849d94
ishitagit2003 <ishitagupta2003@gmail.com>
2024-06-19 23:55:24 +0530

    db

--- api/models/user.js
+++ api/models/user.js
@@ -1,1 +1,12 @@
 const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    
+  },
+  { timestamps: true }
+);
+
+const User = model("blog", userSchema);
+
+module.exports = User;

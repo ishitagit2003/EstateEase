0d659c5d5292565cc8ba8718562628b88eaeb8e3
ishitagit2003 <ishitagupta2003@gmail.com>
2024-06-21 03:22:33 +0530

    authentication

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import  User from '../models/user.js';
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
-    
+
     try {
     const hashedPassword = await bcrypt.hash(password, 10);
     console.log(hashedPassword);
@@ -23,11 +24,56 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {
 
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Find the user by username
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials!" });
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid Credentials!" });
+    }
+
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    //token= payload and secret_Key
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: false,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+    
+    // Exclude the password from the user object before sending it in the response
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure:true,
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to log in!" });
+  }
 };
 
-export const logout = async (req, res) => {
 
+export const logout = async (req, res) => {
+    res.clearCookie("token").status(200).json({ message: "Logout Successful" });
 };
 

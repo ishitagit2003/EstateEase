ff22879e7ae65d1edca31898b8fedd92361672c9
ishitagit2003 <ishitagupta2003@gmail.com>
2024-06-22 02:29:39 +0530

    login register integrated

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import authRoute from "./routes/auth.route.js";
@@ -11,7 +12,7 @@ mongoose.connect(process.env.DATABASE_URL)
 .then(() => console.log("Mongodb connected!"))
   .catch((error) => console.error('Error connecting to Mongodb:', error));
 
-
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoute);

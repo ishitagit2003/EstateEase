14965df1975b25dbf6be3cc2b71a75172cc00098
ishitagit2003 <ishitagupta2003@gmail.com>
2024-06-22 23:48:54 +0530

    login test

--- api/app.js
+++ api/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -12,10 +16,15 @@ mongoose.connect(process.env.DATABASE_URL)
 .then(() => console.log("Mongodb connected!"))
   .catch((error) => console.error('Error connecting to Mongodb:', error));
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(express.json());
+  app.use(cors({ origin: process.env.DATABASE_URL, credentials: true }));
+  app.use(express.json());
+  app.use(cookieParser());
+  
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8800, ()=>{
     console.log("Server is running");

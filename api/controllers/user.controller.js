799b579f67b001e1e18395efb7b06415f2d6ad61
ishitagit2003 <ishitagupta2003@gmail.com>
2024-07-03 23:48:28 +0530

    routing

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,127 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get users!" });
+  }
+};
+
+export const getUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get user!" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+  const { password, avatar, ...inputs } = req.body;
+
+  if (id !== tokenUserId) {
+    return res.status(403).json({ message: "Not Authorized!" });
+  }
+
+  let updatedPassword = null;
+  try {
+    if (password) {
+      updatedPassword = await bcrypt.hash(password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+        id,
+        {
+          ...inputs,
+          ...(updatedPassword && { password: updatedPassword }),
+          ...(avatar && { avatar }),
+        },
+        { new: true }
+      ).select('-password');
+
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to update users!" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+
+  if (id !== tokenUserId) {
+    return res.status(403).json({ message: "Not Authorized!" });
+  }
+
+  try {
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ message: "User deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to delete users!" });
+  }
+};
+
+export const savePost = async (req, res) => {
+    const postId = req.body.postId;
+    const tokenUserId = req.userId;
+  
+    try {
+      const savedPost = await SavedPost.findOne({
+        userId: tokenUserId,
+        postId,
+      });
+  
+      if (savedPost) {
+        await SavedPost.findByIdAndDelete(savedPost._id);
+        res.status(200).json({ message: "Post removed from saved list" });
+      } else {
+        await SavedPost.create({
+          userId: tokenUserId,
+          postId,
+        });
+        res.status(200).json({ message: "Post saved" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to save post!" });
+    }
+  };
+
+  export const profilePosts = async (req, res) => {
+    const tokenUserId = req.userId;
+    try {
+      const userPosts = await Post.find({ userId: tokenUserId });
+      const saved = await SavedPost.find({ userId: tokenUserId }).populate('postId');
+  
+      const savedPosts = saved.map(item => item.postId);
+      res.status(200).json({ userPosts, savedPosts });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to get profile posts!" });
+    }
+  };
+
+  export const getNotificationNumber = async (req, res) => {
+    const tokenUserId = req.userId;
+    try {
+      const number = await Chat.countDocuments({
+        userIDs: tokenUserId,
+        seenBy: { $ne: tokenUserId },
+      });
+      res.status(200).json(number);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to get notification number!" });
+    }
+  };
